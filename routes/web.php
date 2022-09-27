1c71cac9f12b13d950862d3508791336697a5937
torone89 <torone89>
2022-09-27 13:34:34 +0200

    Fix:PostController

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Auth::routes(['register' => false]);
 Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
 
-    // Route::resource('posts', 'PostController');
+    Route::resource('posts', 'PostController');
 
     Route::get('/{any}', function () {
         abort('404');
