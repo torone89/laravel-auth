e3799dc2f85bf71a0030dbd6ce69a68b56d6266a
torone89 <torone89>
2022-09-27 16:25:54 +0200

    feat: add alert confirmation

--- public/js/app.js
+++ public/js/app.js
@@ -37302,6 +37302,8 @@ module.exports = function(module) {
  */
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+__webpack_require__(/*! ./delete_confirmation */ "./resources/js/delete_confirmation.js");
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -37346,6 +37348,25 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/***/ }),
+
+/***/ "./resources/js/delete_confirmation.js":
+/*!*********************************************!*\
+  !*** ./resources/js/delete_confirmation.js ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// Chiedere conferma ogni qualvolta l'utente tenta di cancellare un post.
+var deleteForms = document.querySelectorAll('.delete-form');
+deleteForms.forEach(function (form) {
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var hasConfirmed = confirm('Sei sicuro di voler eliminare questo post?');
+    if (hasConfirmed) form.submit();
+  });
+});
 
 /***/ }),
 
