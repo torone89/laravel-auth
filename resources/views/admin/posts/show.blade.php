2e031a4e24a41459aef38b3da5f1f49b31f42fb0
torone89 <torone89>
2022-09-27 16:04:48 +0200

    feat Add Show

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <header>
+            <h1>{{ $post->title }}</h1>
+            <div>
+                @if ($post->image)
+                    <img class="float-left mr-2" src="{{ $post->image }}" alt="{{ $post->slug }}">
+                @endif
+                <p>{{ $post->content }}</p>
+            </div>
+            <time>Creato il: {{ $post->created_at }}</time><br>
+            <time>Modificato il: {{ $post->updated_at }}</time>
+        </header>
+        <footer class="d-flex align-items-center justify-content-end">
+            <div>
+                <a href="{{ route('admin.posts.index') }}" class="btn btn-secondary ">
+                    <i class="fa solid fa-rotate-left "></i> Indietro
+                </a>
+            </div>
+            <div class="d-flex align-items-center justify-content-end">
+                <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger mx-2" type="submit">
+                        <i class="fa-solid fa-trash m-1"></i>Elimina
+                    </button>
+                </form>
+            </div>
+        </footer>
+    </div>
+@endsection
