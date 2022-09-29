16b894f4c60e1e64fc41c39a3389f1cc082556ec
torone89 <torone89>
2022-09-29 17:06:21 +0200

    feat:add Route nav-item admin.users.index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,10 @@
                             <a class="nav-link @if (Route::is('admin.posts.index')) active @endif"
                                 href="{{ route('admin.posts.index') }}">Posts</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link @if (Route::is('admin.users.index')) active @endif"
+                                href="{{ route('admin.users.index') }}">Utenti</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
