5c1e74931b510aa0a247dfe18f83c2344e2f635d
torone89 <torone89>
2022-09-29 16:59:45 +0200

    feat: add  UserDetailSeeder::class,

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             UserSeeder::class,
+            UserDetailSeeder::class,
             PostSeeder::class
         ]);
     }
